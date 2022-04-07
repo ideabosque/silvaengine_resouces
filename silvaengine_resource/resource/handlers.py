aa717b6c507e845422d59e635a66b0a31818396a
Barry Liu <barry.y.liu@hotmail.com>
2022-04-07 23:15:29 +0800

    [U] Update

--- silvaengine_resource/resource/handlers.py
+++ silvaengine_resource/resource/handlers.py
@@ -119,8 +119,6 @@ def add_resource_handler(cloud_function_name, apply_to, packages):
                     for action in mutation_actions:
                         mutations += _get_operations(config.get(action), True)
 
-                    print(mutations)
-
                     try:
                         ResourceModel(resource_id, service).delete()
                     except:
