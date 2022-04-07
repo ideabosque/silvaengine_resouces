f5d9410f6b1e9b2664d15fa933c1aec2268b59cd
Barry Liu <barry.y.liu@hotmail.com>
2022-04-07 23:02:25 +0800

    [U] Update

--- silvaengine_resource/resource/models.py
+++ silvaengine_resource/resource/models.py
@@ -42,7 +42,7 @@ class BaseModel(Model):
 class ResourceOperationItemMap(MapAttribute):
     label = UnicodeAttribute()
     action = UnicodeAttribute()
-    visible = NumberAttribute(default=1)
+    # visible = NumberAttribute(default=1)
 
 
 class ResourceOperationMap(MapAttribute):
